010e2746c04bc67edcac7ce3b916025c0229fcd7
David Alberto Medina Medina <david00medina@gmail.com>
2018-05-18 11:19:09 +0100

    First Commit

--- run.py
+++ run.py
@@ -19,23 +19,11 @@ print "\nBranch and Bound : "
 print path.path()
 path = [x for x in reversed(path.path())]
 print "Ruta : %s --> %d Expansiones" %(path, expansion)
-#(path, expansion) = search.depth_sorted_graph_search(ab)
-#path = path.path()
-#print "Ruta : %s --> %d Expansiones" %(path, expansion)
 
 print "\nSubestimated Branch and Bound : "
 (path, expansion) = search.subestimated_branch_and_bound_tree_search(ab)
 path = [x for x in reversed(path.path())]
 print "Ruta : %s --> %d Expansiones" %(path, expansion)
-#(path, expansion) = search.depth_heuristic_graph_search(ab)
-#path = path.path()
-#print "Ruta : %s --> %d Expansiones" %(path, expansion)
-
-#print search.depth_sorted_tree_search(ab).path()
-#print search.iterative_deepening_search(ab).path()
-#print search.depth_limited_search(ab).path()
-
-#print search.astar_search(ab).path()
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418

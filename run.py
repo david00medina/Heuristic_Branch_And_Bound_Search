1f7119e6dec52af1ca3dfe29328aeac4ae8d6ccb
David Alberto Medina Medina <david00medina@gmail.com>
2018-05-18 15:42:56 +0100

    First Commit

--- run.py
+++ run.py
@@ -5,23 +5,23 @@ import search
 ab = search.GPSProblem('A', 'B', search.romania)
 
 print "Breadth Search : "
-(path, expansion) = search.breadth_first_graph_search(ab)
+path, expansion = search.breadth_first_graph_search(ab)
 path = [x for x in reversed(path.path())]
 print "Ruta : %s --> %d Expansiones" %(path, expansion)
 
 print "\nDepth Search : "
-(path, expansion) = search.depth_first_graph_search(ab)
+path, expansion = search.depth_first_graph_search(ab)
 path = [x for x in reversed(path.path())]
 print "Ruta : %s --> %d Expansiones" %(path, expansion)
 
 print "\nBranch and Bound : "
-(path, expansion) = search.branch_and_bound_tree_search(ab)
+path, expansion = search.branch_and_bound_graph_search(ab)
 print path.path()
 path = [x for x in reversed(path.path())]
 print "Ruta : %s --> %d Expansiones" %(path, expansion)
 
 print "\nSubestimated Branch and Bound : "
-(path, expansion) = search.subestimated_branch_and_bound_tree_search(ab)
+path, expansion = search.subestimated_branch_and_bound_graph_search(ab)
 path = [x for x in reversed(path.path())]
 print "Ruta : %s --> %d Expansiones" %(path, expansion)
 
